37be5c8cbd09a370e9321269eb34a2cf40ac0d70
pandapanda135 <pandas135works@gmail.com>
2025-05-19 13:38:05 +0100

    Implement NeuroAction.cs

---
+++ Neuro-SDK-Csharp/Actions/BaseNeuroAction.cs
@@ -0,0 +1,59 @@
+using Neuro_SDK_Csharp.Json;
+using Neuro_SDK_Csharp.Websocket;
+
+namespace Neuro_SDK_Csharp.Actions;
+
+public abstract class BaseNeuroAction : INeuroAction
+{
+    public ActionWindow? ActionWindow { get; private set; }
+
+    protected BaseNeuroAction()
+    {
+        ActionWindow = null;
+    }
+
+    public abstract string Name { get; }
+    protected abstract string Description { get; }
+    protected abstract JsonSchema? Schema { get; }
+
+    public virtual bool CanAddToActionWindow(ActionWindow actionWindow) => true;
+
+    ExecutionResult INeuroAction.Validate(ActionData actionData, out object? ResultData)
+    {
+        ExecutionResult result = Validate(actionData, out ResultData);
+
+        if (ActionWindow != null)
+        {
+            // return ActionWindow.Result(result) // not implemented method yet
+            return ExecutionResult.Failure("sdafsdaf");
+        }
+
+        return result;
+    }
+
+    Task INeuroAction.Execute(object? data) => Execute(data);
+    
+    public virtual WSAction GetWsAction()
+    {
+        // return new WSAction(Name, Description, Schema); need to make this
+        throw new NotImplementedException();
+    }
+
+    protected abstract ExecutionResult Validate(ActionData actionData, out object? ResultData);
+    protected abstract Task Execute(object? data);
+
+    void INeuroAction.SetActionWindow(ActionWindow actionWindow)
+    {
+        if (ActionWindow != null)
+        {
+            if (ActionWindow == actionWindow)
+            {
+                ExecutionResult.Failure("Cannot set the action window for this action as it is already set");
+            }
+            
+            return;
+        }
+
+        ActionWindow = actionWindow;
+    }
+}
